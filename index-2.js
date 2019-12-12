85a0ccc95973624c233e274652bf193003a8c9da
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-12 08:23:14 -0800

    index

---
+++ index-2.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    if (request.url === '/about') {
+        return response.end('THE ABOUT PAGE')
+    } else if (request.url === '/contact') {
+        return response.end('THE CONTACT PAGE')
+    } else if (request.url === '/') {
+        return response.end('THE HOME PAGE')
+    } else {
+        response.writeHead(404)
+
+        response.end('THE PAGE WAS NOT FOUND.')
+    }
+})
+
+server.listen(3000)
